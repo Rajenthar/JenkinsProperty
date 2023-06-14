c4a6abe32eb5cb1decb95505394e0a7f75cadd52
Rajenthar <rajenthar.15@cse.mrt.ac.lk>
2023-06-14 14:11:47 +0800

    Create JenkinsPropertyApplicationTests.java

---
+++ src/test/java/com/example/JenkinsProperty/JenkinsPropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsProperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
